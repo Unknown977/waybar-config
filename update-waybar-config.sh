adee8ed3086c48783dcdd81cf68c30b713c763cb
Clark <kentclark321123@gmail.com>
2025-08-12 23:03:40 +0800

    Update waybar config 2025-08-12

--- update-waybar-config.sh
+++ update-waybar-config.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# Save this as ~/bin/update-waybar-config.sh and make it executable with chmod +x
-
 cd /home/clark/.config/waybar
 git add .
 git commit -m "Update waybar config $(date +%Y-%m-%d)"
